d30fa75775b2824c70dba8875ebca06ba47f5d51
Nick Rycar <rycar@chef.io>
2015-04-10 12:23:07 -0700

    Submitting a new change

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures simple-web'
 long_description 'Installs/Configures simple-web'
-version          '0.1.11'
+version          '0.1.12'
 
 depends 'apt'
 depends 'httpd'
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,6 @@ httpd_config 'simple' do
 end
 
 file '/var/www/index.html' do
-  content "<h1>Hello, world!</h1>"
+  content "<h1>Hello, Chef Freinds!</h1>"
   action :create
 end
