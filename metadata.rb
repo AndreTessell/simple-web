1b845f2e0b443b2f94fe9df39469582249be9c46
Nick Rycar <rycar@chef.io>
2015-04-15 12:19:54 -0700

    Reset to hello world

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license          'all_rights'
 description      'Installs/Configures simple-web'
 long_description 'Installs/Configures simple-web'
-version          '0.1.12'
+version          '0.1.13'
 
 depends 'apt'
 depends 'httpd'
