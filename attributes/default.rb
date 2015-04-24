74783a0112414ac8b16a56d03ba03324aaa59baf
Bakh Inamov <b@chef.io>
2015-04-24 10:32:40 -0700

    latest and greatest

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['simple-web']['company-name'] = 'Chef Software, Inc'
+default['simple-web']['company-name'] = 'Best Company Ever'
 default['simple-web']['project-name'] = 'Fueling the love of delivery, one demo at a time.'
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'admin@chef.io'
 license          'all_rights'
 description      'Installs/Configures simple-web'
 long_description 'Installs/Configures simple-web'
-version          '0.3.0'
+version          '0.3.1'
 
 depends 'apt'
 depends 'httpd'
