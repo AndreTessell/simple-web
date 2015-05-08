9019bbadfce58655169b6588eab887f1a7cb0955
Matt Stratton <matt.stratton@gmail.com>
2015-05-08 08:30:48 -0500

    Update for Optum Demo

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'admin@chef.io'
 license          'all_rights'
 description      'Installs/Configures simple-web'
 long_description 'Installs/Configures simple-web'
-version          '0.3.8'
+version          '0.3.999999999'
 
 depends 'apt'
 depends 'httpd'
