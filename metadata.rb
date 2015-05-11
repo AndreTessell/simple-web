124f4f5db60624d4198e39f977f4e4cbbbacca23
Atul <atul@chef.io>
2015-05-11 16:50:29 +0530

    my first delivery demo

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'admin@chef.io'
 license          'all_rights'
 description      'Installs/Configures simple-web'
 long_description 'Installs/Configures simple-web'
-version          '0.3.10'
+version          '0.3.11'
 
 depends 'apt'
 depends 'httpd'
