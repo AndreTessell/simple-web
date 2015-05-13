d268ee3e799064da9d09d6f44ccc3435546c8a48
Matt Stratton <matt.stratton@gmail.com>
2015-05-12 19:31:03 -0500

    Add stuff for chef meetup group

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['simple-web']['company-name'] = 'Chef'
-default['simple-web']['project-name'] = 'Chef Magic Sauce'
+default['simple-web']['company-name'] = 'Chef User Group'
+default['simple-web']['project-name'] = 'Deep Dish Devops'
